ced8a2c484e2581d55b8d16c9999aaedf4a9d1ca
Tai Nyirenda <tai.nyirenda@mobilise.cloud>
2023-06-12 19:41:11 +0100

    Initial commit

---
+++ SunsetTimesCurrent.py
@@ -0,0 +1,22 @@
+import requests
+from datetime import datetime
+
+MY_LAT = 51.507351
+MY_LONG = -0.127758
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0,
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+data = response.json()
+sunrise = data["results"]["sunrise"]
+sunset = data["results"]["sunset"]
+
+print(sunrise.split("T")[1].split(":")[0])
+
+time_now = datetime.now()
+print(time_now.hour)
